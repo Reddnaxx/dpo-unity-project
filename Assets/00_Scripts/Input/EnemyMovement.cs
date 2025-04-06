a7eef1c6b1f86976acdb16abb55420bc18187fff
mylongcode <mylongcode@gmail.com>
2025-04-06 18:53:39 +0300

    add enemy movement

---
+++ Assets/00_Scripts/Input/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._00_Scripts.Input
+{
+    public class EnemyFollow : MonoBehaviour
+    {
+        [SerializeField] public Transform player; 
+        [SerializeField] public float speed = 2f;
+
+        private void Update()
+        {
+            if (player == null) return;
+
+            Vector3 direction = (player.position - transform.position).normalized;
+
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+}

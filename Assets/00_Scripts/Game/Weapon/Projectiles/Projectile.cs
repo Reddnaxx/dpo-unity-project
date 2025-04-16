23b48fac830d258b93333f653f39221f7df7ee66
Александр Рукавишников <as.rukavishnikov@mail.ru>
2025-04-16 16:47:33 +0500

    Bow enhance

--- Assets/00_Scripts/Game/Weapon/Projectiles/Projectile.cs
+++ Assets/00_Scripts/Game/Weapon/Projectiles/Projectile.cs
@@ -2,16 +2,23 @@ using System;
 using _00_Scripts.Game.Entity;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _00_Scripts.Game.Weapon.Projectiles
 {
   public abstract class Projectile : MonoBehaviour
   {
+    public float Damage => _damageModifier * baseDamage;
+    public float Velocity => _velocityModifier * baseVelocity;
+    
     [SerializeField] private float baseDamage = 10f;
     
     [SerializeField] private float lifetime = 5f;
-    [SerializeField] private float velocity = 10f;
+    [SerializeField] private float baseVelocity = 10f;
     [SerializeField] private bool destroyOnHit = true;
+
+    private float _damageModifier = 1f;
+    private float _velocityModifier = 1f;
 
     public IObservable<Vector2> OnHit => _onHit.AsObservable();
 
@@ -35,7 +42,17 @@ namespace _00_Scripts.Game.Weapon.Projectiles
     {
       StartLifetimeTimer();
 
-      _rb.linearVelocity = transform.right * velocity;
+      _rb.linearVelocity = transform.right * Velocity;
+    }
+    
+    public void SetDamageModifier(float modifier)
+    {
+      _damageModifier = modifier;
+    }
+
+    public void SetVelocityModifier(float modifier)
+    {
+      _velocityModifier = modifier;
     }
 
     private void StartLifetimeTimer() => Observable
@@ -53,7 +70,7 @@ namespace _00_Scripts.Game.Weapon.Projectiles
       
       if (collision.gameObject.TryGetComponent<Character>(out var character))
       {
-        character.TakeDamage(baseDamage);
+        character.TakeDamage(Damage);
       }
 
       if (destroyOnHit)
